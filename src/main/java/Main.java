f5d5431d7a768a36e3d05047c359c037c64a1b7b
domshi <90562421+domshi@users.noreply.github.com>
2024-09-10 23:04:38 +0000

    Project progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,29 @@
+/* COP 2800
+  This is an egg program
+  By Dom
+  */
 
+//scanner is for user input gathering
+import java.util.Scanner;
 
-
+//main file aka class
+public class Main {
+  public static void main(String[] args) {
+    
+    // input for number of total chickens and roosters
+    System.out.println("How many chickens do you have? ");
+    Scanner numberOfChickens = new Scanner(System.in);
+    int chickens = numberOfChickens.nextInt();
+    System.out.println("How many roosters do you have? ");
+    Scanner numberOfRoosters = new Scanner(System.in);
+    int roosters = numberOfRoosters.nextInt();
+    
+    // calculation for number of hens provided info given
+    int hens = chickens - roosters;
+    System.out.println("You have " + hens + " hens.");
+    
+  }
+}
 
 
 
